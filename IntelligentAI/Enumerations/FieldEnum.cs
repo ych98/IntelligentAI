068f35f7c7763ae8a4104d1a635d9987e4a8b32c
ych98 <haessen98@163.com>
2024-08-19 18:44:45 +0800

    新增脉络搜索模式

--- IntelligentAI/Enumerations/FieldEnum.cs
+++ IntelligentAI/Enumerations/FieldEnum.cs
@@ -13,7 +13,7 @@ public class FieldEnum : Enumeration
 {
     public static FieldEnum Article = new FieldEnum(0, nameof(Article), "articlesequenceid,paperid,papername,revision,page,paperdate,parenttitle,title,subtitle,editor,contenttxt,url,keyword,updatetime,createtime,itemtype,maineditor,createindex_time,region,viocesize,imagesource,imagecontent,markinfo,paperno,articletype,memo1,memo3,videosize,foreignuniquekey,contentwordscount,sourceurl,class1,videourl,imagesourcecount,watchcount,scenetags,literaturetype,tags,sameid3,place.ns,place.ns1,place.ns2,place.ns3,aiabs,aititle,aieventdate");
 
-    public static FieldEnum Topic = new FieldEnum(1, nameof(Topic), "articlesequenceid,_score,degree,title,contenttxt,url,updatetime,papername,markinfo,class1,createtime,tags,same_id,sameid3,articletype");
+    public static FieldEnum Topic = new FieldEnum(1, nameof(Topic), "articlesequenceid,_score,degree,title,contenttxt,url,updatetime,papername,markinfo,class1,createtime,tags,same_id,sameid3,articletype,aiabs,aititle,aieventdate");
 
     public static FieldEnum Vector = new FieldEnum(2, nameof(Vector), "title,paperdate,fwtype,updatetime,mediaid,aid,paperid");
 
